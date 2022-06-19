5914ca1e7f9b7bd994fe3416b280acb95a2d1a51
Your Name <you@example.com>
2022-06-19 10:06:17 +0200

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  const apiKey = "eA5sAhbADxoIlDM4gSKkgsfuhWvYOqTtPakQOG4T";
+  const today = new Date()
+  const todayDate = today.getFullYear()+'-'+(String(today.getMonth()+1).padStart(2,'0'))+'-'+today.getDate()
+  const displayDate = today.toLocaleString('en-uk',{day:'numeric', month:'long', year:'numeric'})
+  let requestedDate = todayDate
+
+
+  async function fetchData() { 
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${requestedDate}`)
+    const responseJson = await response.json();
+    console.log(responseJson)
+    setData(responseJson);
+  };
+
+  useEffect(() => {
+     fetchData()
+  }, []);
+
+
+
+
+
+
   return (
     <div className="App">
     </div>
